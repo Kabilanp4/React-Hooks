2f192e3f581d58c4ed721e4ced98f7ae600f2359
Kabilan P <104247633+Kabilanp4@users.noreply.github.com>
2024-09-20 14:37:18 +0530

    UseEffect Hook Implementation

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
-import { useState } from "react";
-import { FaStar } from "react-icons/fa";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-const createArray = (length) => [...Array(length)];
-
-function Star({ selected = false, onSelect }) {
-  return <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />;
-}
-const StartComponent = ({ totalStars }) => {
-  const [star, setStar] = useState(0);
-  return (
-    <>
-      {createArray(totalStars).map((d, i) => (
-        <Star key={i} selected={star > i} onSelect={() => setStar(i + 1)} />
-      ))}
-      <p>
-        selected {star} out of {totalStars}
-      </p>
-    </>
-  );
-};
 function App() {
-  return (
-    <div className="App">
-      <StartComponent totalStars={5} />
-    </div>
-  );
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch(`https://api.github.com/users`)
+      .then((response) => response.json())
+      .then((res) => setData(res));
+  }, []);
+
+  if (data) {
+    return (
+      <>
+        <ul>
+          {data.map((user) => (
+            <li key={user.id}> {user.login}</li>
+          ))}
+        </ul>
+        <button onClick={() => setData([])}>Remove Data</button>
+      </>
+    );
+  }
 }
 
 export default App;
