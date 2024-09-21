32af34cc0999c59fa43be2f4f00ec1c28d640358
Kabilan P <104247633+Kabilanp4@users.noreply.github.com>
2024-09-21 11:39:27 +0530

    useContext Hook Implementation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
+import { useTrees } from ".";
 import "./App.css";
-import { useInput } from "./useInput";
-function App() {
-  const [soundProps, resetSoundProps] = useInput("");
-  const [colorProps, resetColorProps] = useInput("#000000");
 
-  const submit = (e) => {
-    e.preventDefault();
-    alert(`${soundProps.value} sounds like ${colorProps.value}`);
-    resetSoundProps();
-    resetColorProps();
-  };
+function App() {
+  const { trees } = useTrees();
   return (
     <>
-      <form onSubmit={submit}>
-        <input {...soundProps} type="text" placeholder="Sound..." />
-        <input {...colorProps} type="color" />
-        <button>Add</button>
-      </form>
+      <p>The React Hooks i've learned so far:</p>
+      <ul>
+        {trees.map((data) => (
+          <li key={data.id}>{data.type}</li>
+        ))}
+      </ul>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { createContext, useContext } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+const TreeesContext = createContext();
+export const useTrees = () => {
+  useContext(TreeesContext);
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const trees = [
+  { id: "1", type: "useState" },
+  { id: "2", type: "useEffect" },
+  { id: "3", type: "useReducer" },
+  { id: "4", type: "useRef" },
+  { id: "5", type: "useContext" },
+];
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <TreeesContext.Provider value={{ trees }}>
     <App />
-  </React.StrictMode>
+  </TreeesContext.Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
